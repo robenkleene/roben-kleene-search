76d099b8d026daa2c90664fccac2218ea48e837c
Roben Kleene <contact@robenkleene.com>
2020-09-23 07:08:35 -0400

    Revert "Start over"
    This reverts commit acbd2feba59cfaabaf887d6a12da33db33a5e000.

---
+++ src/components/Search/Search.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Search.css';
+
+const Search = () => (
+  <div className="Search">
+    <input type="text" />
+  </div>
+);
+
+Search.propTypes = {};
+
+Search.defaultProps = {};
+
+export default Search;
