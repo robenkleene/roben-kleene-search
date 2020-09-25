7e2cc6d92a37444748639354d3010ae072f68ba4
Roben Kleene <contact@robenkleene.com>
2020-09-25 07:08:32 -0400

    Reference engines component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Search from "components/Search/Search"
+import Engines from "components/Engines/Engines"
 
 function App() {
   return (
     <div className="App">
       <Search />
+      <Engines />
     </div>
   );
 }
