890c266c7cb97bdb5298d0b7d020e0ad6ea1f7d9
Roben Kleene <contact@robenkleene.com>
2020-10-13 06:41:58 -0400

    Add data

--- src/data.js
+++ src/data.js
@@ -1,6 +1,33 @@
 export const enginesData = {
+  "duckduckgo Lucky": "http://www.duckduckgo.com/?q=%s",
   "Cycling 74": "https://duckduckgo.com/?q=%s+site%3Acycling74.com",
-  "Audiobus": "https://duckduckgo.com/?q=%s+site%3Aforum.audiob.us",
+  Audiobus: "https://duckduckgo.com/?q=%s+site%3Aforum.audiob.us",
   "Acclaimed Music": "https://duckduckgo.com/?q=%s+site%3Aacclaimedmusic.net",
-  "Ableton": "https://duckduckgo.com/?q=%s+site%3Aforum.ableton.com"
+  Ableton: "https://duckduckgo.com/?q=%s+site%3Aforum.ableton.com",
+  AllMusic: "https://duckduckgo.com/?q=%s+site%3Aallmusic.com",
+  CDM: "https://duckduckgo.com/?q=%s+site%3Acdm.link",
+  "Apple Developer": "https://duckduckgo.com/?q=%s+site%3Adeveloper.apple.com",
+  "Daring Fireball": "https://duckduckgo.com/?q=%s+site%3Adaringfireball.net",
+  "Designer News": "https://duckduckgo.com/?q=%s+site%3Adesignernews.co",
+  Editor:
+    "https://google.com/search?q=%s+site%3Anytimes.com+OR+site%3Anewyorker.com+OR+site%3Awsj.com",
+  GitHub: "https://duckduckgo.com/?q=%s+site%3Agithub.com",
+  "Hacker News": "https://duckduckgo.com/?q=%s+site%3Anews.ycombinator.com",
+  "Indie Hackers": "https://duckduckgo.com/?q=%s+site%3Aindiehackers.com",
+  "Michael Tsai": "https://duckduckgo.com/?q=%s+site%3Amjtsai.com",
+  MacStories: "https://duckduckgo.com/?q=%s+site%3Awww.macstories.net+&ia=web",
+  "Native Instruments Forum":
+    "https://duckduckgo.com/?q=%s+site%3Awww.native-instruments.com/forum/",
+  Pitchfork: "https://duckduckgo.com/?q=%s+site%3Apitchfork.com",
+  "Resident Advisor": "https://duckduckgo.com/?q=%s+site%3Aresidentadvisor.net",
+  Reddit: "https://duckduckgo.com/?q=%s+site%3Areddit.com",
+  "Stack Exchange All":
+    "https://google.com/search?q=site%3Astackexchange.com+OR+site%3Aaskubuntu.com+OR+site%3Aserverfault.com+OR+site%3Asuperuser.com+OR+site%3Astackoverflow.com",
+  "Stack Overflow": "https://duckduckgo.com/?q=%s+site%3Astackoverflow.com",
+  "Tech Stack":
+    "https://www.google.com/search?q=%s+site%3Astateofjs.com+OR+site%3Astateofcss.com+OR+site%3Athoughtworks.com%2Fradar%2F",
+  "Uses This": "https://duckduckgo.com/?q=%s+site%3Ausesthis.com",
+  "Wikipedia Lucky": "https://duckduckgo.com/?q=%s+site%3Awikipedia.org",
+  Twitter: "https://twitter.com/search?q=%s",
+  Wikipedia: "https://duckduckgo.com/?q=%s+site%3Awikipedia.org",
 };
