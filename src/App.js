ec3d3fcbe682bc53e4ee0abdf8e379021500d7af
Roben Kleene <contact@robenkleene.com>
2020-11-11 11:40:02 +0000

    Remove engines

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from "react";
 import "./App.css";
 import Search from "components/Search/Search";
-import Engines from "components/Engines/Engines";
 
 function App() {
   return (
     <div className="App">
       <Search />
-      <Engines />
     </div>
   );
 }
